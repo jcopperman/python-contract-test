30a73037db7843169bd880298b171cbfc9109f37
Jonathan Opperman <jonathan.opperman@thesustainables.nl>
2023-09-22 14:26:31 +0200

    Fixed syntax error

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import requests
 
 def get_data():
-    response = requests.get("http://example.com/api/data"")
+    response = requests.get("http://example.com/api/data")
     return response.json()
 
 # Get the JSON response and print it to the console
